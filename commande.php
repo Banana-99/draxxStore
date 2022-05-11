706004964d0ee30895f52518d352bdb6d23f3936
San Lamamba P <Sanlamamba05@gmail.com>
2022-05-11 20:23:35 +0000

    stock app

--- commande.php
+++ commande.php
@@ -4,29 +4,12 @@
 include_once("./include/aside.php");
 ?>
 <?php
-$client = new Client();
+$fournisseurs = new Fournisseur();
 $stock = new Stock();
-$cart = new Cart();
 $commande = new Commande();
+$cart = new Cart();
+$clients = new Client();
 
-$cart_elements = array();
-if (isset($_GET['cart'])) {
-    $cart = $_GET['cart'];
-}
-
-if (isset($_POST['product'])) {
-    // if cart get issset then add panier else create panier
-    if (isset($_GET['cart'])) {
-        $cart->addCart($_POST['product'], $_POST['quantity'], $_GET['cart']);
-    } else {
-        // create a commande 
-        $commande_id = $commande->addCommande(date("Y-m-d"), "en cours");
-        // add cart to commande
-        $cart->addCart($_POST['product'], $_POST['quantity'], $commande_id);
-        // header to redirect to commande page with the commande id
-        header("Location: commande.php?cart=$commande_id");
-    }
-}
 ?>
 
 <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
@@ -38,46 +21,6 @@ if (isset($_POST['product'])) {
         <div class="row">
             <div class="col-12">
                 <div class="row">
-                    <div class="col-6">
-                        <div class="card mb-4">
-                            <div class="card-header pb-0 d-flex justify-content-between align-items-center">
-                                <h4> Choisir un produit</h4>
-                            </div>
-                            <div class="card-body">
-                                <!-- form with a dropdown and a text and a button -->
-                                <form method="post">
-                                    <div class="form-group">
-                                        <label for="product">Produit</label>
-                                        <select required class="form-control" id="product" name="product">
-                                            <option value="">Choisir un produit</option>
-                                            <?php
-                                            $products = $stock->getStock();
-                                            if (!empty($products)) {
-                                                foreach ($products as $product) {
-                                            ?>
-                                            <option value="<?php echo $product['id']; ?>">
-                                                <?php echo $product['designation']; ?>
-                                            </option>
-                                            <?php
-                                                }
-                                            }
-                                            ?>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="quantity">Quantité</label>
-                                        <input type="number" class="form-control" id="quantity" name="quantity"
-                                            placeholder="Quantité">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">
-                                        Ajouter au panier
-                                    </button>
-                                </form>
-
-                            </div>
-                        </div>
-                    </div>
-
                     <div class="card mb-4">
                         <div class="card-header pb-0 d-flex justify-content-between align-items-center">
                             <h4>Liste des commandes</h4>
@@ -87,13 +30,55 @@ if (isset($_POST['product'])) {
                                 <table class="table table-striped">
                                     <thead>
                                         <tr>
-                                            <th>Nom prodit</th>
+                                            <th>N Commande</th>
+                                            <th>Produit</th>
+                                            <th>Date de livraison </th>
+                                            <th>Client </th>
                                             <th> Prix unitaire</th>
                                             <th>Quantité</th>
                                             <th>Total</th>
                                         </tr>
                                     </thead>
                                     <tbody>
+                                        <?php
+                                        $commandes = $commande->getCommandes();
+                                        if (!empty($commandes)) {
+                                            foreach ($commandes as $commande) {
+                                                // for each commande, get the corresponding nCommande from cart
+                                                $nCommande = $commande['nCommande'];
+
+                                                $cart_curr = $cart->getCartByNCommande($nCommande);
+                                                foreach ($cart_curr as $cart_curr) { ?>
+                                        <tr>
+                                            <td> <?php echo $commande['id']; ?> </td>
+                                            <td> <?php
+                                                                // get the corresponding produit from stock
+                                                                $produit = $stock->getStockByID($cart_curr['produit_id']);
+                                                                echo $produit['designation'];
+
+                                                                ?> </td>
+                                            <td> <?php echo $commande['date_livraison']; ?> </td>
+                                            <td> <?php
+                                                                // get the corresponding client from client
+                                                                $client = $clients->getClientByID($commande['client_id']);
+                                                                echo $client['nom'];
+                                                                echo " ";
+                                                                echo $client['prenom'];
+                                                                ?> </td>
+                                            <td> <?php
+                                                                // get the corresponding produit from stock
+                                                                $produit = $stock->getStockByID($cart_curr['produit_id']);
+                                                                echo $produit['prix'];
+
+                                                                ?> </td>
+                                            <td> <?php echo $cart_curr['quantite']; ?> </td>
+                                            <td> <?php
+                                                                echo $cart_curr['quantite'] * $produit['prix'];
+                                                                ?> </td>
+                                        </tr>
+                                        <?php }
+                                            }
+                                        } ?>
 
                                     </tbody>
                                 </table>
@@ -102,55 +87,6 @@ if (isset($_POST['product'])) {
                     </div>
                 </div>
 
-                <div class="col-12">
-                    <div class="card mb-4">
-                        <div class="card-header pb-0 d-flex justify-content-between align-items-center">
-                            <h4>Panier de la commande en cours</h4>
-                            <div>
-                                <a href="#" class="btn btn-primary">
-                                    Panier
-                                    <span>
-                                        <i class="fa-plus-circle"></i>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="card-body px-0 pt-0 pb-0">
-                            <div class="table-responsive p-0">
-                                <table class="table table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Référence Produit</th>
-                                            <th>Prix Unitaire</th>
-                                            <th>Quantité</th>
-                                            <th>Montant Total (Fcfa)</th>
-                                            <th>Total du panier (Fcfa)</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                Geraud
-                                            </td>
-                                            <td>
-                                                CEO
-                                            </td>
-                                            <td>
-                                                Retraité
-                                            </td>
-                                            <td>
-                                                01/05/1980
-                                            </td>
-                                            <td>
-                                                01/05/1980
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
             </div>
             <?php include_once("./include/footer.php"); ?>
